d169d54f0b3fe8627a6c61a870d02f01085ef15e
Eugene <eugene.kotsarev@gmail.com>
2021-06-02 00:05:12 +0300

    upd php script

--- contact.php
+++ contact.php
@@ -1,16 +1,27 @@
 <?php
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 
-if (isset($_POST['email'])) {
+if (isset($_POST) && $contentType === "application/json") {
+  $requestBody = file_get_contents('php://input');
+  $requestBody = json_decode($requestBody, true);
+
+  if ($requestBody === null) {
+    header('HTTP/1.1 400 Bad Request');
+    echo json_encode([
+      'errorMessage' => 'Please provide valid JSON',
+    ]);
+    die;
+  }
 
   // EDIT THE 2 LINES BELOW AS REQUIRED
   $email_to = "gimmethenick@gmail.com";
   $email_subject = "Поступило новое предложение о покупке домена ahml1.ru";
 
 
-  $name = $_POST['name']; // required
-  $email_from = $_POST['email']; // required
-  $telephone = $_POST['phone']; // not required
-  $comments = $_POST['comments']; // required
+  $name = trim($requestBody['name']); // required
+  $email_from = trim($requestBody['email']); // required
+  $telephone = trim($requestBody['phone']); // not required
+  $comments = trim($requestBody['message']); // required
 
 
   $email_message = "Детали сообщения ниже.\n\n";
@@ -30,55 +41,21 @@ if (isset($_POST['email'])) {
   $headers = 'From: ' . $email_from . "\r\n" .
     'Reply-To: ' . $email_from . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
-  @mail($email_to, $email_subject, $email_message, $headers);
 
-  ?>
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Sales Inquery || [Your Domain]</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Mukta+Mahee:300,700" rel="stylesheet">
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"/>
-    <link rel="stylesheet" href="css/style.css"/>
-  </head>
-  <body>
-  <section class="bg-alt hero p-0">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="bg-faded col-sm-6 text-center col-fixed">
-          <div class="vMiddle">
-            <h1 class="pt-4 h2">
-              <span>Thank you for offer, I will contact as soon as possible. Cheers !!!</span>
-            </h1>
-            <div class="row d-md-flex text-center justify-content-center text-primary action-icons">
-              <div class="col-sm-4">
-                <p><em class="ion-ios-telephone-outline icon-md"></em></p>
-              </div>
-              <div class="col-sm-4">
-                <p><em class="ion-ios-chatbubble-outline icon-md"></em></p>
-                <p class="lead"><a href="mailto:the.saroz@gmail.com">hey@sarozpoddar.com.np</a></p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 offset-sm-6 px-0">
-          <section class="bg-alt">
-            <div class="row height-100">
-              <div class="col-sm-8 offset-sm-2 mt-2">
-                <h1 class="pt-4 h2"><span class="text-green">Евгений Коцарев</span></h1>
-                <span class="text-muted">беларусь</span>
-                <p><span>UX/UI Designer & Front-end Developer</span></p>
-              </div>
-            </div>
-          </section>
-        </div>
-      </div>
-    </div>
-  </section>
-  </body>
-  </html>
-<?php } ?>
+  $success = @mail($email_to, $email_subject, $email_message, $headers);
+
+  if ($success) {
+    http_response_code(200);
+
+    // Json Header
+    header('Content-Type: application/json');
+    // return changed $requestBody
+    echo json_encode($requestBody);
+  } else {
+    http_response_code(400);
+    echo "Ошибка. Данные не отправлены.";
+  }
+
+  die;
+}
+?>
